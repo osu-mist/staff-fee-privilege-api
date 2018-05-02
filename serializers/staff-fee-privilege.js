0d7410b8c57dfd08589fb08dcf8934f4fc9e4a63
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-05-02 09:32:39 -0700

    Use yaml instead of yml

--- serializers/staff-fee-privilege.js
+++ serializers/staff-fee-privilege.js
@@ -8,7 +8,7 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 const api = config.get('api');
 
 // Read attributes from swagger and adjust them to match oracledb column names
-const swagger = yaml.safeLoad(fs.readFileSync('./swagger.yml', 'utf8'));
+const swagger = yaml.safeLoad(fs.readFileSync('./swagger.yaml', 'utf8'));
 keys = _.keys(swagger.definitions.StaffFeePrivilegeRourceObject.properties.attributes.properties);
 _.forEach(keys, (key, index) => {
   keys[index] = decamelize(key).toUpperCase();
