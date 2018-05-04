17681101e00016cc5e98e0f3851fac38b61c9a4d
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-05-04 14:54:26 -0700

    Update unit test

--- tests/db.test.js
+++ tests/db.test.js
@@ -6,23 +6,20 @@ const { assert, expect } = chai;
 chai.use(chaiString);
 
 describe('Test sanitize', () => {
-  it('should return correct format', (done) => {
-    const rawRow = {
-      CAMPUS: ' Oregon State - Corvallis',
-    };
-    const sanitizedRow = sanitize(rawRow);
+  const rawRow = {
+    CAMPUS: ' Oregon State - Corvallis  ',
+    CURRENT_ENROLLED: 'Y',
+    CURRENT_REGISTERED: 'N',
+  };
+  const sanitizedRow = sanitize(rawRow);
 
+  it('should return correct format', (done) => {
     expect(sanitizedRow.CAMPUS).not.to.startsWith(' ');
     expect(sanitizedRow.CAMPUS).not.to.endsWith(' ');
     done();
   });
 
   it('should return correct type', (done) => {
-    const rawRow = {
-      CAMPUS: '  Oregon State - Corvallis  ',
-    };
-    const sanitizedRow = sanitize(rawRow);
-
     assert.isBoolean(sanitizedRow.CURRENT_ENROLLED);
     assert.isBoolean(sanitizedRow.CURRENT_REGISTERED);
     done();
