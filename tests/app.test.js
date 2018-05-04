c8b8a349bb313dc478463633f7bfe0b98afbdc57
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-05-04 09:56:32 -0700

    Integrate HTTP test

--- tests/app.test.js
+++ tests/app.test.js
@@ -8,19 +8,19 @@ const { expect } = chai;
 const { authentication } = config;
 
 // Genric HTTP test cases
-const httpAssertion = (res, err, status, contentType) => {
+const httpAssertion = (res, status, contentType) => {
   expect(res).to.have.status(status);
   expect(res).to.have.header('content-type', contentType);
 };
 
-describe('GET /staff-fee-privilege', () => {
+describe('HTTP integration tests', () => {
   it('should return 200 (OK)', (done) => {
     chai.request(app)
       .get('/staff-fee-privilege')
       .auth(authentication.username, authentication.password)
       .query({ term: '201801' })
       .end((err, res) => {
-        httpAssertion(res, err, 200, 'application/json; charset=utf-8');
+        httpAssertion(res, 200, 'application/json; charset=utf-8');
         done();
       });
   });
@@ -30,7 +30,7 @@ describe('GET /staff-fee-privilege', () => {
       .get('/staff-fee-privilege')
       .auth(authentication.username, authentication.password)
       .end((err, res) => {
-        httpAssertion(res, err, 400, 'application/json; charset=utf-8');
+        httpAssertion(res, 400, 'application/json; charset=utf-8');
         done();
       });
   });
@@ -39,36 +39,17 @@ describe('GET /staff-fee-privilege', () => {
     chai.request(app)
       .get('/staff-fee-privilege')
       .end((err, res) => {
-        httpAssertion(res, err, 401, 'application/json; charset=utf-8');
+        httpAssertion(res, 401, 'application/json; charset=utf-8');
         done();
       });
   });
-});
 
-describe('GET /staff-fee-privilege/:id', () => {
-  it('should return 200 (OK)', (done) => {
-    chai.request(app)
-      .get('/staff-fee-privilege/930686277')
-      .auth(authentication.username, authentication.password)
-      .end((err, res) => {
-        httpAssertion(res, err, 200, 'application/json; charset=utf-8');
-        done();
-      });
-  });
-  it('should return 401 (Unauthorized)', (done) => {
-    chai.request(app)
-      .get('/staff-fee-privilege/:id')
-      .end((err, res) => {
-        httpAssertion(res, err, 401, 'application/json; charset=utf-8');
-        done();
-      });
-  });
   it('should return 404 (Not Found)', (done) => {
     chai.request(app)
       .get('/staff-fee-privilege/123456789')
       .auth(authentication.username, authentication.password)
       .end((err, res) => {
-        httpAssertion(res, err, 404, 'application/json; charset=utf-8');
+        httpAssertion(res, 404, 'application/json; charset=utf-8');
         done();
       });
   });
