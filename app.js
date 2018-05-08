3a4a518b0283c90379490b2581c9977b4133598e
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-05-07 17:02:53 -0700

    Add logger (morgan)

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const config = require('config');
 const express = require('express');
 const fs = require('fs');
 const https = require('https');
+const moment = require('moment');
+const morgan = require('morgan');
+const path = require('path');
+const rfs = require('rotating-file-stream');
 const _ = require('lodash');
 const { getStaffFeePrivilegesByTerm, getStaffFeePrivilegesById } = require('./contrib/contrib');
 const { getStaffFeePrivilegesBy } = require('./db/db');
@@ -22,6 +26,20 @@ const httpsOptions = {
   secureProtocol: server.secureProtocol,
 };
 const httpsServer = https.createServer(httpsOptions, app);
+
+// Use logger middleware with standard Apache combined log format
+const logger = config.get('logger');
+const api = config.get('api');
+const logsDirectory = path.join(__dirname, logger.logsDirectory);
+if (!fs.existsSync(logsDirectory)) {
+  fs.mkdirSync(logsDirectory);
+}
+const logGenerator = () => `${api.name}-${moment.utc().format('MM-DD-YYYY')}.log`;
+const logStream = rfs(logGenerator, {
+  interval: '1m',
+  path: logsDirectory,
+});
+app.use(morgan('combined', { stream: logStream }));
 
 // Basic authentication middleware
 const { username, password } = config.authentication;
