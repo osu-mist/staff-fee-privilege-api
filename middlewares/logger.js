48525eaf4d99d8e136e04b4ee8d9d3b86804596a
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-08-10 13:05:26 -0700

    Reset

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -6,28 +6,25 @@ require('winston-daily-rotate-file');
 const loggerConfig = config.get('logger');
 const apiName = config.get('api').name;
 
-let logger;
-if (loggerConfig.enable) {
-  // Transport for daily rotate file
-  const dailyRotateFileTransport = new (winston.transports.DailyRotateFile)({
-    filename: `${apiName}-%DATE%.log`,
-    datePattern: loggerConfig.pattern,
-    maxSize: loggerConfig.size,
-    zippedArchive: loggerConfig.archive,
-    dirname: loggerConfig.path,
-  });
+// Transport for daily rotate file
+const dailyRotateFileTransport = new (winston.transports.DailyRotateFile)({
+  filename: `${apiName}-%DATE%.log`,
+  datePattern: loggerConfig.pattern,
+  maxSize: loggerConfig.size,
+  zippedArchive: loggerConfig.archive,
+  dirname: loggerConfig.path,
+});
 
-  // Transport for console output
-  const consoleTransport = new winston.transports.Console({
-    json: loggerConfig.jsonConsole,
-    colorize: loggerConfig.colorize,
-  });
+// Transport for console output
+const consoleTransport = new winston.transports.Console({
+  json: loggerConfig.jsonConsole,
+  colorize: loggerConfig.colorize,
+});
 
-  logger = expressWinston.logger({
-    transports: [dailyRotateFileTransport, consoleTransport],
-    expressFormat: true,
-    colorize: loggerConfig.colorize,
-  });
-}
+const logger = expressWinston.logger({
+  transports: [dailyRotateFileTransport, consoleTransport],
+  expressFormat: true,
+  colorize: loggerConfig.colorize,
+});
 
 module.exports = { logger };
