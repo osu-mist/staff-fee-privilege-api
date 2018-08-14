489fdd2d002b84a8a48b579784ada55617624026
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-08-14 15:57:31 -0700

    CO-1097 Load test
    1. Increase thread pool size according to maxPool
    2. Fix unhandled bug
    3. Update readme

--- db/db.js
+++ db/db.js
@@ -8,6 +8,12 @@ process.on('SIGINT', () => process.exit());
 
 oracledb.outFormat = oracledb.OBJECT;
 const dbConfig = config.get('database');
+
+// Increase 1 extra thread for every 5 pools
+const threadPoolSize = dbConfig.poolMax + (dbConfig.poolMax / 5);
+process.env.UV_THREADPOOL_SIZE = threadPoolSize > 128 ? 128 : threadPoolSize;
+
+// Create pool
 const poolPromise = oracledb.createPool(dbConfig);
 
 // Get connection from created pool
@@ -49,6 +55,12 @@ const getStaffFeePrivilegesByQuery = query =>
 const getStaffFeePrivilegesById = query =>
   new Promise(async (resolve, reject) => {
     const connection = await getConnection();
+
+    // Should return 404 if id cannot be parsed as osuId and term code
+    if (!query.osuId || !query.term) {
+      resolve(undefined);
+    }
+
     try {
       const { rows } = await connection.execute(
         contrib.getStaffFeePrivilegesByQuery(query),
