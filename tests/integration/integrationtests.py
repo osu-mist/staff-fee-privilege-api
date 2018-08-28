3d33d6d693d13e5940a06d3c232d8ecb4608707a
Tso-Liang Wu <TsoLiang.Wu@oregonstate.edu>
2018-08-28 11:20:48 -0700

    Test with random ids

--- tests/integration/integrationtests.py
+++ tests/integration/integrationtests.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import unittest
+from random import randint
 
 import utils
 
@@ -11,21 +12,27 @@ class TestGetByParameters(unittest.TestCase):
         logging.debug(f"Request took {elapsed_seconds} second(s)")
         self.assertLess(elapsed_seconds, max_elapsed_seconds)
 
-    def test_get_by_valid_osu_id(self):
-        res = utils.get_by_osu_id(osu_id)
-        res_data = res.json()['data']
+    def test_get_by_osu_id(self):
+        for _ in range(3):
+            random_osu_id = f"93{randint(0000000, 9999999)}"
+            with self.subTest(random_osu_id=random_osu_id):
+                res = utils.get_by_osu_id(random_osu_id)
+                res_data = res.json()['data']
 
-        self.assert_response_time(res.elapsed.total_seconds(), 1)
-        self.assertEqual(res.status_code, 200)
-        self.assertIsInstance(res_data, list)
+                self.assert_response_time(res.elapsed.total_seconds(), 1)
+                self.assertEqual(res.status_code, 200)
+                self.assertIsInstance(res_data, list)
 
     def test_get_by_term_id(self):
-        res = utils.get_by_term_id(term_id)
-        res_data = res.json()['data']
+        for _ in range(3):
+            random_term_id = f"{randint(2000, 2018)}0{randint(0, 3)}"
+            with self.subTest(random_term_id=random_term_id):
+                res = utils.get_by_term_id(random_term_id)
+                res_data = res.json()['data']
 
-        self.assert_response_time(res.elapsed.total_seconds(), 1)
-        self.assertEqual(res.status_code, 200)
-        self.assertIsInstance(res_data, list)
+                self.assert_response_time(res.elapsed.total_seconds(), 20)
+                self.assertEqual(res.status_code, 200)
+                self.assertIsInstance(res_data, list)
 
 
 # class TestGetByID(unittest.TestCase):
